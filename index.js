50eb736d7e20b00a629600201617512fa114efd0
github username <cmyerskeitt@gmail.com>
2022-05-19 08:08:03 -0400

    refactor

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function sumTwoSmallestNumbers(numbers) {  
-    let sorted = numbers.sort((a, b) => a - b)
-    return sorted[0] + sorted[1]
+    numbers.sort((a, b) => a - b)
+    return numbers[0] + numbers[1]
 }
